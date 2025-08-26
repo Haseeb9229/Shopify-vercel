5559e404ccad7ea5a9a944756de0d63384c89594
Haseeb Ahmed <haseebahmed9229@gmail.com>
2025-08-26 19:43:38 +0500

    cros issue

--- api/get-sso.js
+++ api/get-sso.js
@@ -1,8 +1,23 @@
 const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    return res.status(200).end();
+  }
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   try {
-    const response = await fetch("https://saml-sso.improvlearning.com/gettoken");
+    const response = await fetch(
+      "https://saml-sso.improvlearning.com/gettoken"
+    );
 
     if (!response.ok) {
       return res.status(500).json({ error: "Failed to fetch token" });
--- api/order-count-by-order-id.js
+++ api/order-count-by-order-id.js
@@ -1,6 +1,19 @@
 const fetch = require("node-fetch");
 
 module.exports = async (req, res) => {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    return res.status(200).end();
+  }
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   const { id } = req.query;
 
   if (!id) {
